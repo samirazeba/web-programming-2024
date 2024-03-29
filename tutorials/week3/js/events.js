96082c797f2565939091b26956ced1222ecf179c
zebasamira <samira.zeba56@gmail.com>
2024-03-30 00:26:19 +0100

    test commit

---
+++ tutorials/week3/js/events.js
@@ -0,0 +1,8 @@
+sayHello = () => {
+    alert("HELLO");
+}
+let button = document.getElementById("myButton");
+button.addEventListener("click", () => {
+    let outputPrint = document.getElementById("output-print");
+    outputPrint.innerHTML = "<h1>Button has been clicked</h1>";
+});
